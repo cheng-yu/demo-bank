61ac7d65d17b6abcb696d47b75101b4f3cf77e82
蔡承佑 <tim.tsai@kdanmobile.com>
2024-08-11 03:16:16 +0800

    Add create account method

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -1,11 +1,21 @@
 const AccountService = require('../services/accountService');
 
-const getAccounts = async (req, res) => {
+const getAccounts = (req, res) => {
   const accountService = new AccountService();
-  const accounts = await accountService.getAccountList();
+  const accounts = accountService.getAccountList();
   res.json(accounts);
+};
+
+const createAccount = (req, res) => {
+  const accountService = new AccountService();
+  const account = accountService.createAccount({ 
+    name: req.body.name, 
+    balance: req.body.balance 
+  });
+  res.json(account);
 };
 
 module.exports = {
   getAccounts,
+  createAccount
 };
--- src/routes/accountRoutes.js
+++ src/routes/accountRoutes.js
@@ -4,6 +4,7 @@ const accountController = require('../controllers/accountController');
 const router = express.Router();
 
 router.get('/', accountController.getAccounts);
+router.post('/', accountController.createAccount);
 
 module.exports = router;
 
